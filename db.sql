ac5a39c599fce984c59bb858b1d637f275c7ea88
Pratham Shelar <shelarpratham80@gmail.com>
2022-08-10 14:33:52 +0530

    Some Changes

--- db.sql
+++ db.sql
@@ -2,7 +2,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE USERS(userid uuid DEFAULT uuid_generate_v1(), name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL, bio TEXT,profile_img TEXT, PRIMARY KEY(userid));
 
-SELECT (userid,name,email,password,bio) FROM USERS;
 
 CREATE TABLE BLOGS(blogid uuid DEFAULT uuid_generate_v1(), userid uuid NOT NULL, title TEXT NOT NULL, description TEXT NOT NULL, author TEXT NOT NULL, category TEXT NOT NULL, createdon TEXT NOT NULL, totalviews INT NOT NULL, blog_img TEXT, keywords TEXT NOT NULL,author_img TEXT,PRIMARY KEY(blogid));
 
